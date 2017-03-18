dd280c9b41cc5b4791206131357297f02fc46a8c
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2017-03-18 18:11:05 -0400

    Add base exercise files from Exercism.io.

---
+++ Exercism/Gigasecond/Gigasecond.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.Gigasecond
+{
+    public static class Gigasecond
+    {
+        public static DateTime Date(DateTime birthDate)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
