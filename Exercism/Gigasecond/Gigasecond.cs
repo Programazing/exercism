d606240b00174ce7796864e02a3eea353d146a05
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2017-03-22 16:36:04 -0400

    Clean up Gigasecond and make Date one line.

--- Exercism/Gigasecond/Gigasecond.cs
+++ Exercism/Gigasecond/Gigasecond.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exercism.Gigasecond
 {
     public static class Gigasecond
     {
-        public static DateTime Date(DateTime birthDate)
-        {
-            return birthDate.AddSeconds(1000000000);
-        }
+        public static DateTime Date(DateTime birthDate) => birthDate.AddSeconds(1000000000);
     }
 }
