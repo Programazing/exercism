05c90f3710513b574ce69482debd864c13d8028f
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2017-03-23 11:57:44 -0400

    Refactor SumOfSquares to be a one line method using linq.

--- Exercism/DifferenceOfSquares/DifferenceOfSquares.cs
+++ Exercism/DifferenceOfSquares/DifferenceOfSquares.cs
@@ -6,20 +6,7 @@ public static class Squares
 {
     public static double SquareOfSums(int max) => Math.Pow(Enumerable.Range(1, max).Sum(), 2);
 
-    public static double SumOfSquares(int max)
-    {
-        var range = Enumerable.Range(1, max);
-        Double sum = 0.0;
-
-
-        foreach (var number in range)
-        {
-
-            sum += Math.Pow(number, 2);
-        }
-
-        return sum;
-    }
+    public static double SumOfSquares(int max) => Enumerable.Range(1, max).Select(n => n * n).Sum();
 
     public static double DifferenceOfSquares(int max) => SquareOfSums(max) - SumOfSquares(max);
 }
