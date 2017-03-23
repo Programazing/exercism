d3d53e3c2bf227068a857be49b21a2729c7d6513
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2017-03-23 12:07:31 -0400

    Add Exercism.DifferenceOfSquaresTests Namespace to DifferenceOfSquares.cs.

--- Exercism/DifferenceOfSquares/DifferenceOfSquares.cs
+++ Exercism/DifferenceOfSquares/DifferenceOfSquares.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public static class Squares
+namespace Exercism.DifferenceOfSquaresTests
 {
-    public static double SquareOfSums(int max) => Math.Pow(Enumerable.Range(1, max).Sum(), 2);
+    public static class Squares
+    {
+        public static double SquareOfSums(int max) => Math.Pow(Enumerable.Range(1, max).Sum(), 2);
 
-    public static double SumOfSquares(int max) => Enumerable.Range(1, max).Select(n => n * n).Sum();
+        public static double SumOfSquares(int max) => Enumerable.Range(1, max).Select(n => n * n).Sum();
 
-    public static double DifferenceOfSquares(int max) => SquareOfSums(max) - SumOfSquares(max);
+        public static double DifferenceOfSquares(int max) => SquareOfSums(max) - SumOfSquares(max);
+    } 
 }
