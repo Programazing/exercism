6a9c352f6a578e0b255f791030bb4a7f5b5ff112
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2017-03-22 22:14:06 -0400

    Add initial solution to find the square of sums to SquareOfSums.

--- Exercism/DifferenceOfSquares/DifferenceOfSquares.cs
+++ Exercism/DifferenceOfSquares/DifferenceOfSquares.cs
@@ -1,11 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Squares
 {
-    public static int SquareOfSums(int max)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int SquareOfSums(int max) => Enumerable.Range(0, max + 1).Sum() * Enumerable.Range(0, max + 1).Sum();
 
     public static int SumOfSquares(int max)
     {
